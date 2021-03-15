45659980834303e632150c9dc90ba239ed6632ad
vmwhoami <vmwhoami@gmail.com>
2021-03-15 17:05:09 +0200

    Style the logo and the navbar

--- src/components/Errorpage.js
+++ src/components/Errorpage.js
@@ -1,3 +1,13 @@
-const Errorpage = () => <h2>There has been an error loading your data</h2>;
+import css from '../styles/error.module.css';
+import { ErroIcon } from './Svgs';
+
+const Errorpage = () => (
+  <div className={css.container}>
+    <div className={css.errorcontainer}>
+      <ErroIcon style={css.icon} />
+      <h2>There has been an error loading your data</h2>
+    </div>
+  </div>
+);
 
 export default Errorpage;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from '../styles/nav.module.css';
 import PriceFilter from '../container/PriceFilter';
+import { LogoSvg } from './Svgs';
 
 const Nav = () => (
   <nav className={css.nav}>
     <div className={css.container}>
+      <NavLink to="/" className={css.logoLink}>
+        <LogoSvg style={css.logo} />
+      </NavLink>
       <PriceFilter />
       <NavLink to="/" className={css.link}>
         <span>Home</span>
