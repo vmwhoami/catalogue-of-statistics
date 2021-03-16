2761a4a329f0ce709330ee25edb8a4bfe81c4d42
vmwhoami <vmwhoami@gmail.com>
2021-03-16 14:08:05 +0200

    Add testing for actions

--- src/__TESTS__/nav.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Nav from '../components/Nav';
-
-it('renders Nav correctly', () => {
-  const tree = renderer.create(<Nav handleOnSelect={jest.fn()} />);
-  expect(tree).toMatchSnapshot();
-});
